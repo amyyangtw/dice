e7de563fb496d538ccbc067fbedfc437da4d9f6e
Amy Yang <tzufangtw8@gmail.com>
2019-12-23 20:14:43 +0800

    Updates

---
+++ precache-manifest.db7b04af18e66991d9d76f898b5d7a55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de60cc3535fc9f74c214e9dfa04d522e",
+    "url": "/dice/index.html"
+  },
+  {
+    "revision": "437814345e9101ee96eb",
+    "url": "/dice/static/css/main.c926445b.chunk.css"
+  },
+  {
+    "revision": "e421447d6efa109aa487",
+    "url": "/dice/static/js/2.038fb812.chunk.js"
+  },
+  {
+    "revision": "437814345e9101ee96eb",
+    "url": "/dice/static/js/main.4ec9bfcd.chunk.js"
+  },
+  {
+    "revision": "57ba4dd0abb3a4debf78",
+    "url": "/dice/static/js/runtime-main.05e0e9ff.js"
+  }
+]);
